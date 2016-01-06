e2d8479cc7507219ddca77dd3bfdc5c2f4b3dbfc
Peter Greth <peter.greth@gmx.de>
2016-01-06 16:17:33 +0100

    Added basic main menu and networking example

---
+++ Assets/Mainmenu/Scripts/MainPanel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainPanel : MonoBehaviour {
+
+	public NetworkManager nm;
+	public string gameScene;
+	public Canvas canvas;
+
+	public void Update(){
+		if (nm.numPlayers == 2) {
+			changeLevel ();
+		}
+
+	}
+	public void OnClickHost() {
+		nm.StartHost ();
+	}
+
+	public void OnClickJoin() {
+		nm.StartClient();
+		changeLevel ();
+	}
+
+	void changeLevel(){
+		nm.ServerChangeScene ("game");
+		canvas.enabled = false;
+		this.enabled = false;
+	}
+
+}
---
+++ Assets/Mainmenu/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerMovement : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer)
+			return;
+		
+		var x = Input.GetAxis("Horizontal")*0.1f;
+		var z = Input.GetAxis("Vertical")*0.1f;
+
+		transform.Translate(x, 0, z);
+	}
+}
