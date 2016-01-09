d56e9e384333b1e4db79412eae04cb1247d8cd5b
Lukas <Bone008@users.noreply.github.com>
2016-01-09 13:49:46 +0100

    Added quick ability to look behind the safe wall for testing purposes (hold Alt)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,23 +19,26 @@ public class Player : MonoBehaviour
     // false = player 1, true = player 2
     public bool isPlayer2 = false;
 
-	public bool useCactus = false;
+    public bool useCactus = false;
 
-	// index of the safe in focus
-	[HideInInspector]
-	public int focusedSafe = 0;
+    // index of the safe in focus
+    [HideInInspector]
+    public int focusedSafe = 0;
 
-	private InputIF input;
+    private InputIF input;
 
     void Start()
     {
-		if (useCactus) {
-			this.gameObject.AddComponent<CactusController> ();
-			input = this.gameObject.GetComponent<CactusController> ();
-		} else {
-			this.gameObject.AddComponent<HomeController> ();
-			input = this.gameObject.GetComponent<HomeController> ();
-		}
+        if (useCactus)
+        {
+            this.gameObject.AddComponent<CactusController>();
+            input = this.gameObject.GetComponent<CactusController>();
+        }
+        else
+        {
+            this.gameObject.AddComponent<HomeController>();
+            input = this.gameObject.GetComponent<HomeController>();
+        }
     }
 
     void Update()
@@ -52,21 +55,36 @@ public class Player : MonoBehaviour
         // far left/right key pressed -> focus next active safe
         do
         {
-			if (input.FarRightPressed ())
+            if (input.FarRightPressed())
             {
                 focusedSafe = (focusedSafe + 1) % safeCount;
             }
-			if (input.FarLeftPressed ())
+            if (input.FarLeftPressed())
             {
                 focusedSafe = (focusedSafe + safeCount - 1) % safeCount;
             }
         } while (!level.safes[focusedSafe].IsActive);
 
-        // smoothly move cam to focussed safe
-        transform.position = Vector3.Lerp(transform.position, level.safes[focusedSafe].transform.position + cameraOffset, Time.deltaTime * cameraSpeed);
+
+        // smoothly move cam to focussed safe (or behind it if cheating)
+        Vector3 targetPosition = level.safes[focusedSafe].transform.position + cameraOffset;
+
+        // cheat for testing
+        if(Input.GetKey(KeyCode.LeftAlt))
+        {
+            // move behind the safe
+            targetPosition -= 2 * cameraOffset;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.identity;
+        }
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * cameraSpeed);
+
 
         // send input to safe
-		level.safes[focusedSafe].SetInputState(isPlayer2, input.getInput());
+        level.safes[focusedSafe].SetInputState(isPlayer2, input.getInput());
     }
 
 }
