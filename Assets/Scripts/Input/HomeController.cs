cc1e505a776711973e189d9c61d794520c29be75
Peter Greth <peter.greth@gmx.de>
2016-01-10 00:04:46 +0100

    Added analog-input simulation for homecontroller

--- Assets/Scripts/Input/HomeController.cs
+++ Assets/Scripts/Input/HomeController.cs
@@ -10,10 +10,28 @@ public class HomeController : MonoBehaviour, InputIF
     public bool FarLeftPressed { get { return Input.GetKeyDown(KeyCode.LeftArrow); } }
     public bool FarRightPressed { get { return Input.GetKeyDown(KeyCode.RightArrow); } }
 
+	private float leftSliderValue = 0.0f;
+	private float rightSliderValue = 0.0f;
+
+	private float leftKnobValue = 0.0f;
+	private float rightKnobValue = 0.0f;
+
+	private float microphoneValue = 0.0f;
+
     void Update()
     {
         inputState = collectInput();
     }
+
+	void OnGUI() {
+		leftSliderValue = GUI.VerticalSlider (new Rect (20, 25, 15, 100), leftSliderValue, 1.0f, 0.0f);
+		rightSliderValue = GUI.VerticalSlider (new Rect (65, 25, 15, 100), rightSliderValue, 1.0f, 0.0f);
+
+		leftKnobValue = GUI.HorizontalSlider (new Rect (5, 5, 40, 15), leftKnobValue, 0.0f, 1.0f);
+		rightKnobValue = GUI.HorizontalSlider (new Rect (50, 5, 40, 15), rightKnobValue, 0.0f, 1.0f);
+
+		microphoneValue = GUI.VerticalSlider (new Rect (Screen.width - 20, 5, 15, 100), microphoneValue, 1.0f, 0.0f);
+	}
 
     private InputState collectInput()
     {
@@ -21,6 +39,14 @@ public class HomeController : MonoBehaviour, InputIF
 
         inputState.HeldButtons = checkButtonsHeld();
         inputState.PressedButtons = checkButtonsPressed();
+
+		inputState.SetAnalogInput (GameAnalogInput.LeftSlider, leftSliderValue);
+		inputState.SetAnalogInput (GameAnalogInput.RightSlider, rightSliderValue);
+
+		inputState.SetAnalogInput (GameAnalogInput.LeftKnob, leftKnobValue);
+		inputState.SetAnalogInput (GameAnalogInput.RightKnob, rightSliderValue);
+
+		inputState.SetAnalogInput (GameAnalogInput.Microphone, microphoneValue);
 
         return inputState;
     }
@@ -57,7 +83,5 @@ public class HomeController : MonoBehaviour, InputIF
 
         return flags;
     }
-
-
 
 }
