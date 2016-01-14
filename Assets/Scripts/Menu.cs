6ff8d4fa2245bf43232df4561c68741e73301da2
Lukas <Bone008@users.noreply.github.com>
2016-01-14 11:19:26 +0100

    Added simple main menu.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
