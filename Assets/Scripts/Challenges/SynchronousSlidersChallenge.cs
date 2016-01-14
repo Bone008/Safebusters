9432b74f381978bce3c18f821fed865372d97c8b
Lukas <Bone008@users.noreply.github.com>
2016-01-14 10:28:47 +0100

    Added simple status display to synchronous sliders, #18.

--- Assets/Scripts/Challenges/SynchronousSlidersChallenge.cs
+++ Assets/Scripts/Challenges/SynchronousSlidersChallenge.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SynchronousSlidersChallenge : AbstractChallenge
 {
@@ -9,9 +10,13 @@ public class SynchronousSlidersChallenge : AbstractChallenge
 
     private SliderController leftSliderFront;
     private SliderController rightSliderFront;
+    private RectTransform statusArrowFront;
+    private Text statusTextFront;
 
     private SliderController leftSliderBack;
     private SliderController rightSliderBack;
+    private RectTransform statusArrowBack;
+    private Text statusTextBack;
 
     private bool hasStarted = false; // Wait, until both players set their sliders to 1, then start judging
 
@@ -22,11 +27,15 @@ public class SynchronousSlidersChallenge : AbstractChallenge
         sliders = frontGameObject.GetComponentsInChildren<SliderController>();
         leftSliderFront = sliders[0];
         rightSliderFront = sliders[1];
+        statusArrowFront = frontGameObject.transform.Find("Canvas/Arrow").GetComponent<RectTransform>();
+        statusTextFront = frontGameObject.transform.Find("Canvas/Status").GetComponent<Text>();
 
         // GUI Back
         sliders = backGameObject.GetComponentsInChildren<SliderController>();
         leftSliderBack = sliders[0];
         rightSliderBack = sliders[1];
+        statusArrowBack = backGameObject.transform.Find("Canvas/Arrow").GetComponent<RectTransform>();
+        statusTextBack = backGameObject.transform.Find("Canvas/Status").GetComponent<Text>();
     }
 
     void Update()
@@ -34,7 +43,7 @@ public class SynchronousSlidersChallenge : AbstractChallenge
         // Sync-Challenge -> Both focus required
         if (!hasFocusFront || !hasFocusBack)
         {
-            hasStarted = false;
+            SetStarted(false);
             return;
         }
 
@@ -54,8 +63,7 @@ public class SynchronousSlidersChallenge : AbstractChallenge
         // Check if started
         if(lsf >= START_EPSILON && rsf >= START_EPSILON && lsb >= START_EPSILON && rsb >= START_EPSILON)
         {
-            hasStarted = true;
-            // TODO: do stuff to show the players that the challenge has started
+            SetStarted(true);
         }
 
         // Abort here if not started
@@ -67,11 +75,27 @@ public class SynchronousSlidersChallenge : AbstractChallenge
         {
             safe.SolveChallenge();
         }
-               
         // Check if failed (simple) (distance between corresponding sliders >= max epsilon)
-        if((Math.Abs(lsf - lsb) >= MAX_EPSILON) || (Math.Abs(rsf - rsb) >= MAX_EPSILON))
+        else if((Math.Abs(lsf - lsb) >= MAX_EPSILON) || (Math.Abs(rsf - rsb) >= MAX_EPSILON))
         {
+            SetStarted(false);
             safe.FailChallenge();
         }
     }
+
+    public void SetStarted(bool flag)
+    {
+        hasStarted = flag;
+        if(flag)
+        {
+            statusArrowFront.localRotation = statusArrowBack.localRotation = Quaternion.Euler(0, 0, 270);
+            statusTextFront.text = statusTextBack.text = "DISCHARGE";
+        }
+        else
+        {
+            statusArrowFront.localRotation = statusArrowBack.localRotation = Quaternion.Euler(0, 0, 90);
+            statusTextFront.text = statusTextBack.text = "INIT";
+        }
+    }
+
 }
