6bc9ff25e5c53bf297a82d5d6e00267db2796157
Lukas <Bone008@users.noreply.github.com>
2016-01-24 23:58:59 +0100

    Started working on multi monitor support, #25.

---
+++ Assets/Scripts/DualScreen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DualScreen : MonoBehaviour
+{
+
+    public Camera cam1;
+    public Camera cam2;
+
+    void Start()
+    {
+        if (Display.displays.Length >= 2)
+        {
+            Debug.Log("Enabling dual screen mode.");
+
+            Display.displays[1].Activate();
+            cam1.rect = new Rect(0, 0, 1, 1);
+            cam1.targetDisplay = 0;
+            cam2.rect = new Rect(0, 0, 1, 1);
+            cam2.targetDisplay = 1;
+        }
+    }
+
+}
