6a3d5bb956031c1be2a9ece4a027c76ab81f6f1d
Peter Greth <peter.greth@gmx.de>
2016-01-25 17:37:07 +0100

    I'm sorry

--- Assets/Scripts/ViewControllers/AccelPosController.cs
+++ Assets/Scripts/ViewControllers/AccelPosController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class AccelPosController : MonoBehaviour {
 
-    private const OFFSET_Z = 0.02f;
+    private const float OFFSET_Z = 0.02f;
 	public Transform handle;
 
 	public void SetValue(Vector2 pos)
